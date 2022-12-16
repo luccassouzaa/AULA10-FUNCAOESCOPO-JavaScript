7ccf94ae3897970e86e79ef509f663faab8b5d46
Luccas Andre <luccasmelim@yahoo.com.br>
2022-12-16 16:40:12 -0300

    v1.0

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// GLOBAL
+
+function showCar(){
+    // LOCAL
+    var car = "BMW"
+    console.log(car)
+}
+
+showCar()
+
+// --------------------- //
+
+function sum(number1,number2){
+    var resultado = number1 + number2
+    return resultado
+}
+
+console.log(sum(2,4))
